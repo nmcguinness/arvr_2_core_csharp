15eb97ed08a2be85dd42890cc40a48c482f29a25
niall mcguinness <niall.mcguinness@dkit.ie>
2021-02-27 13:30:50 +0000

    Add project files.

---
+++ arvr_2_core_csharp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Timers;
+
+namespace ARVR
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Program theApp = new Program();
+            theApp.Start();
+        }
+
+        private void Start()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
